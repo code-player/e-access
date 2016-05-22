bf34476f68de11266f580c00a1bebdff6a2c0ff8
code-player <rhythmchopra8@gmail.com>
2016-05-22 14:21:26 +0530

    password protection removed for MySQL XAMPP

--- e-access/core/database/connect.php
+++ e-access/core/database/connect.php
@@ -1,16 +1,16 @@
 <?php
 	$servername = "localhost";
-	$username = "root";
-	$password = "password";
-	
+	$username = "";
+	$password = "";
+
 	function connect_error_page()
 	{	header('Location:../pages/connect_error.php');
 		exit();
 		}
-	
+
 	$conn = mysqli_connect($servername, $username, $password,'e_access');
 	$conn1 = mysqli_connect($servername, $username, $password,'result');
-	
+
 	if (!$conn)
 	   die("Connection failed: ".connect_error_page());
 	if (!$conn1)
